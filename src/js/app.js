c3e12f9faadb6d850e794c0676e2068aaa5520d5
Krishna Chaitanya Kotu <chaitusince1992@gmail.com>
2016-12-10 15:09:17 +0530

    angular input form validations

--- src/js/app.js
+++ src/js/app.js
@@ -6,12 +6,23 @@ app.config(['$routeProvider', function ($routeProvider) {
             templateUrl: "templates/home.template.html"
         })
         .when('/login', {
-            templateUrl: "templates/login.template.html"
+            templateUrl: "templates/login.template.html",
+            controller: 'loginController'
         })
         .when('/dashboard', {
             templateUrl: "templates/dashboard.template.html"
         })
-        .otherwise("/home", {
-            templateUrl: "templates/home.template.html"
+        .otherwise("/login", {
+            templateUrl: "templates/login.template.html",
+            controller: 'loginController'
         })
+}]);
+
+app.controller('loginController', ['$scope', function ($scope) {
+    $scope.submitLogin = function (validFlag) {
+        if (validFlag) {
+            console.log($scope.passwordLogin);
+            console.log($scope.emailLogin)
+        }
+    };
 }]);
